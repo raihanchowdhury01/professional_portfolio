f2bc35ac38b78c1e0206d90619021071e7d7d8ab
RaihanHossan99 <raihan24696@gmail.com>
2024-04-25 14:05:24 +0600

    Intersectionobserver test

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 
 
+// IntersectionObserver design
+
+//1. catch the header text
+const headerText = document.querySelector(".headerText");
+
+// IntersectionObserver interface making part
+const observe = new IntersectionObserver((items)=>{
+    items.forEach(item =>{
+        if(item.isIntersecting){
+            headerText.classList.add("headerTextAction");
+        }else{
+            headerText.classList.remove("headerTextAction");
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+observe.observe(headerText)
+
+
+// action button and others design
 const bar = document.querySelector("#bars");
 const crosse = document.querySelector("#xMark");
 const barClass = document.querySelector(".bars");
@@ -17,3 +39,4 @@ crosse.addEventListener("click", ()=>{
     crosseClass.classList.remove("cActive");
     closeM.classList.remove("activeMenu");
 })
+
