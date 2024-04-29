05b4a9536743177adc3349add38e3799e8451f4f
RaihanHossan99 <raihan24696@gmail.com>
2024-04-29 14:14:48 +0600

    Intersectionobserver created

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 
 
-// IntersectionObserver design
-
-//1. catch the header text
-const headerText = document.querySelector(".headerText");
-
-// IntersectionObserver interface making part
-const observe = new IntersectionObserver((items)=>{
-    items.forEach(item =>{
-        if(item.isIntersecting){
-            headerText.classList.add("headerTextAction");
-        }else{
-            headerText.classList.remove("headerTextAction");
-        }
-    })
-}, {
-    threshold: 0.5
-});
-
-observe.observe(headerText)
-
-
 // action button and others design
 const bar = document.querySelector("#bars");
 const crosse = document.querySelector("#xMark");
@@ -29,14 +8,69 @@ const crosseClass = document.querySelector(".xMark");
 const openM = document.querySelector(".menuBar");
 const closeM = document.querySelector(".menuBar");
 
-bar.addEventListener("click", ()=>{
+bar.addEventListener("click", () => {
     barClass.classList.add("active");
     crosseClass.classList.add("cActive");
     openM.classList.add("activeMenu");
 })
-crosse.addEventListener("click", ()=>{
+crosse.addEventListener("click", () => {
     barClass.classList.remove("active");
     crosseClass.classList.remove("cActive");
     closeM.classList.remove("activeMenu");
 })
+
+// navbar sticky design
+const navbar = document.querySelector(".navBar");
+const menubar = document.querySelector(".menuBar");
+const check = navbar.offsetTop;
+const menu = navbar.offsetTop;
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > check) {
+        navbar.classList.add("sticky");
+        menubar.classList.add("stick");
+    } else {
+        navbar.classList.remove("sticky");
+        menubar.classList.remove("stick");
+    }
+})
+
+// IntersectionObserve animation design
+const content = document.querySelector(".about")
+const experience = document.getElementById("experience");
+const project = document.getElementById("project");
+let counter = 0;
+let projects = 0;
+const observer = new IntersectionObserver((items) => {
+    console.log(items);
+    items.forEach(item => {
+        if (item.isIntersecting) {
+            content.classList.add("aboutAnim");
+            setInterval(() => {
+                if (counter === 3) {
+                    clearInterval();
+                } else {
+                    counter++;
+                    experience.innerHTML = counter;
+                }
+            }, 100);
+            setInterval(() => {
+                if (projects === 20) {
+                    clearInterval();
+                } else {
+                    projects++;
+                    project.innerHTML = projects;
+                }
+            }, 100);
+        } else {
+            content.classList.remove("aboutAnim");
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+observer.observe(content);
+
+// about section counter design
+
 
